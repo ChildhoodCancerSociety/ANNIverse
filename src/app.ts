7e9f547284ab8c241cb279c75083cf1ca3141303
Zane Shaw <zshawdev@gmail.com>
2023-04-01 20:51:53 -0400

    Create initial routing structure

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,48 @@
 import express from "express";
-
 const app = express();
 
-export default app;
+app.route("/users")
+  .get((req, res) => {
+    // get all users
+  })
+
+  .post((req, res) => {
+    // create a user
+  })
+
+  .put((req, res) => {
+    // noop
+  })
+
+  .delete((req, res) => {
+    // noop
+  })
+
+app.route("/users/:userId")
+  .get((req, res) => {
+    // get specific user
+  })
+
+  .post((req, res) => {
+    // noop
+  })
+
+  .put((req, res) => {
+    // edit specific user
+  })
+
+  .delete((req, res) => {
+    // delete specific user
+  })
+
+app.use("/teams", (req, res) => {
+  // ...
+})
+
+app.use("/tasks", (req, res) => {
+  // ...
+})
+
+app.use("/meetings", (req, res) => {
+  // ...
+})
