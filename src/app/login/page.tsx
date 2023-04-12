6b30a51aff2ddeb9f4b22b669b4007d43a753d67
Adrian Garcia Rios <adrigar10@gmail.com>
2023-04-12 19:24:44 -0400

    dev-94-session-management-middleware

---
+++ src/app/login/page.tsx
@@ -0,0 +1,57 @@
+"use client"
+import { useState } from "react";
+import { projectAuth } from "../../firebase/config";
+import { useRouter } from "next/navigation";
+
+//redirect
+export default function Login(){
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const router = useRouter();
+
+    const handleLogin = async(e:React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        try{
+            const response = await fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email, password})
+            });
+            if(response.ok){
+                const idToken = await projectAuth.currentUser?.getIdToken();
+                //get token and send it to server
+
+                console.log("logged in");
+                router.push('/logout');
+            }
+        } catch (error){
+            console.error(error);
+        }
+    }
+
+    return(
+        // <form onSubmit={handleLogin}>
+        //     <h2>Sample Login</h2>
+        //     <label>
+        //         <span>Email:</span>
+        //         <input
+        //             type="email"
+        //             onChange={(e)=> setEmail(e.target.value)}
+        //             value={email}
+        //         />
+        //     </label>
+        //     <label>
+        //         <span>Password:</span>
+        //         <input
+        //             type="password"
+        //             onChange={(e)=> setPassword(e.target.value)}
+        //             value={password}
+        //         />
+        //     </label>
+        //     <button>Login</button>
+        // </form>
+        <div></div>
+    )
+}
---
+++ src/middlewares/validate.ts
@@ -0,0 +1,25 @@
+import { Response, NextFunction } from "express";
+// const jwt = require('jsonwebtoken')
+import admin from "firebase-admin";
+
+const validate = async (req: any, res: Response, next: NextFunction)=> {
+    // const token = req.headers.authorization.split("Bearer ")[1];
+    // if(token){
+    //     const decodedJwt = jwt.verify(token);
+    //     if(decodedJwt){
+    //         req.user = decodedJwt
+    //         next();
+    //     }
+    // }
+    // res.sendStatus(401);
+    try{
+        const token:any = req.headers.authorization?.split('Bearer ')[1];
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        req.user = decodedToken;
+        next();
+    } catch (e){
+        return res.json({message: `Failed to Authenticate`});
+    }
+}
+
+module.exports = validate;
