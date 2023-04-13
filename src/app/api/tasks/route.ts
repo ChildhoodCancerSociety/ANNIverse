1a0ed35e2406ef41c96ed825308ad9f3353bc621
ahkq <christopherromero233@gmail.com>
2023-04-13 01:27:44 -0700

    CRUD operations for /tasks and /tasks/:taskId

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,51 @@
+import { Response, Request, Router } from 'express'
+import prisma from '@/prisma'
+import { Prisma, PrismaClient,} from '@prisma/client'
+import rbac from "@/middlewares/rbac";
+
+export const getTasks = async (req: Request, res: Response) => {
+    const tasks = await prisma.task.findMany();
+    res.json({ tasks })
+}
+
+let newTask: Prisma.TaskCreateInput;
+
+export const createTask = async (req: Request, res: Response) => {
+
+    const createTask = await prisma.task.create({ data: newTask });
+    res.json({ createTask })
+}
+
+export const updateTaskById = async (req: Request, res: Response) => {
+    const teamId = req.params.teamId;
+    const { title, description } = req.body;
+    const updateTask = await prisma.task.update({
+        where: { id: teamId },
+        data: { title, description },
+    })
+    res.json({ updateTask })
+}
+
+export const getTasksById = async (req: Request, res: Response) => {
+    const teamId = req.params.teamId;
+    const getTasks = await prisma.team.findUnique({
+        where: { id: teamId },
+    })
+    res.json({ getTasks })
+}
+
+export const deleteTasksById = async (req: Request, res: Response) => {
+    const teamId = req.params.teamId;
+    const deleteTask = await prisma.task.delete({
+        where: { id: teamId }
+    })
+    res.json({ deleteTask })
+}
+
+const router = Router()
+
+router.get("/", getTasks)
+router.post("/", rbac("Volunteer"), createTask)
+router.get("/:taskId", getTasksById)
+router.put("/:taskId", rbac("Volunteer"), updateTaskById)
+router.delete("/:taskId", rbac("Volunteer"), deleteTasksById)
