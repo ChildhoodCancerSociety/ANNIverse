7dec142665e043cb90f3a73cf187b2c8937d8e3f
lilyshowlader <lilyshowlader@gmail.com>
2023-05-10 20:25:07 -0400

    update user model

--- src/app/api/users/seed.ts
+++ src/app/api/users/seed.ts
@@ -1,16 +1,20 @@
 import { PrismaClient } from '@prisma/client';
-import { Hina_Mincho } from "next/font/google";
-import { measureMemory } from "vm";
+
 
 const prisma = new PrismaClient();
 
 export default async function createUser() {
   const newUser = await prisma.user.create({
     data: {
-      name: 'John Doe',
-      email: 'john.doe@example.com',
+      name: 'Mia Howlader',
+      email: 'miaislost@gmail.com',
       role: 'SoftwareDev',
     },
   })
   console.log(newUser)
 }
+
+createUser()
+  .then(() => console.log('User created successfully!'))
+  .catch((error) => console.error('Error creating user:', error))
+
