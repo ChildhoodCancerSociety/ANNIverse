b45af10f7068bad54e3afbd16a899a505f5e5d65
Adrian Garcia Rios <adrigar10@gmail.com>
2023-05-17 11:57:28 -0400

    Added Discord Login and route handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
-export default async function Home() {
-  return (
-    <div className="h-12 w-12 bg-green-600"></div>
+"use client";
+import Image from 'next/image'
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function Home() {
+  const {data: session} = useSession();
+  
+  if(session){
+    const {user} = session;
+    return (
+          <div>  
+            {user?.image &&(
+              <Image
+                src={user.image}
+                alt=""
+                width={38}
+                height={38}
+              />
+            )}
+            <h1>You are signed in! {user?.name}</h1>
+            <h1>Email: {user?.email}</h1>
+            <button className="bg-green-300 rounded" onClick={() => signOut().catch(console.error)}>Sign out</button>
+          </div>
   );
+}
+
+  return(
+          <div>
+            <div>Not signed in</div>
+            <button className="bg-green-300 rounded" onClick={() => signIn('discord').catch(console.error)}>Sign In</button>
+          </div>
+  )
 }
