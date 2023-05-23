a6ce8c8064c4228b90a60f79ef026dc4481bec43
Ibrahim Saberi <georgeipsum@gmail.com>
2023-05-23 17:35:46 -0400

    add discord packages + boilerplate

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,15 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
+import env from "@/env";
+
 // https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
 
-const { DISCORD_CLIENT_ID = "", DISCORD_CLIENT_SECRET = "" } = process.env;
 const handler = NextAuth({
   providers: [
     DiscordProvider({
-      clientId: DISCORD_CLIENT_ID,
-      clientSecret: DISCORD_CLIENT_SECRET,
+      clientId: env.discordClientId,
+      clientSecret: env.discordClientSecret,
     }),
   ],
 });
