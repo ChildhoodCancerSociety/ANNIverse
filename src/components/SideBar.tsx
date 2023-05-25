48464cc4ebd4ede5ec58f4d201830d8ba5dcfd6c
delcada6882 <adam.delcastillocall6882@stu.mtec.edu>
2023-05-24 21:56:01 -0600

    changed all icons to react-icons and added the tooltip to label each of the icons.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,39 +1,89 @@
+'use client';
+import { useRef } from 'react';
+import { BsPeopleFill, BsListTask, BsAlarmFill, BsPersonCircle } from 'react-icons/bs'
+
 function SideBar() {
-    return(
-        <div className="sideBar h-screen flex flex-col justify-between w-20  outline-neutral-400 items-center shadow-xl hover:shadow-2xl transition-all p-4">
-        <div className="sideBarTop flex flex-col justify-between h-60">
-          <div className="whiteBoard relative">
-            <div className="whiteBoardSvg border-2 border-neutral-400 h-7 w-12 rounded-sm hover:cursor-pointer hover:scale-110 hover:border-neutral-700 transition-all"></div>
-            <h1 className="absolute bottom-0 left-14 text-neutral-50 bg-neutral-700 p-1 rounded-sm opacity-0">Whiteboard</h1>
-          </div>
-          <div className="teams text-neutral-500 hover:cursor-pointer hover:scale-110 hover:text-neutral-700 transition-all flex justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-people-fill" viewBox="0 0 16 16">
-              <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-            </svg>
-          </div>
-          <div className="tasks text-neutral-500 hover:cursor-pointer hover:scale-110 hover:text-neutral-700 transition-all flex justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-list-task" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5H2zM3 3H2v1h1V3z"/>
-              <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9z"/>
-              <path fill-rule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V7zM2 7h1v1H2V7zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H2zm1 .5H2v1h1v-1z"/>
-            </svg>
-          </div>
-          <div className="alarm text-neutral-500 hover:cursor-pointer hover:scale-110 hover:text-neutral-700 transition-all flex justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-alarm-fill" viewBox="0 0 16 16">
-              <path d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zm2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527z"/>
-            </svg>
-          </div>
+  const whiteBoardText = useRef<HTMLParagraphElement>(null);
+  const teamsText = useRef<HTMLParagraphElement>(null);
+  const tasksText = useRef<HTMLParagraphElement>(null);
+  const alarmText = useRef<HTMLParagraphElement>(null);
+  const accountText = useRef<HTMLParagraphElement>(null);
+
+  return (
+    <div className="sideBar h-screen flex flex-col justify-between w-20  outline-neutral-400 items-center shadow-xl hover:shadow-2xl transition-all p-4">
+      <div className="sideBarTop flex flex-col justify-between h-60">
+        <div className="whiteBoard relative text-neutral-500 hover:cursor-pointer hover:scale-110 hover:text-neutral-700 transition-all flex justify-center" 
+          onMouseEnter={() => { 
+            whiteBoardText.current?.classList.add('block'); 
+            whiteBoardText.current?.classList.remove('hidden'); 
+          }}
+          onMouseLeave={() => { 
+            whiteBoardText.current?.classList.add('hidden'); 
+            whiteBoardText.current?.classList.remove('block'); 
+          }}
+        >
+          <div className="whiteBoardSvg border-2 border-neutral-400 h-8 w-14 rounded-sm hover:cursor-pointer hover:border-neutral-700 transition-all" ></div>
+          <p className='top-0 w-28 absolute left-16 bg-neutral-800 p-1 rounded-sm text-neutral-50 text-center hidden' ref={whiteBoardText}>White Board</p>
         </div>
-        <div className="whiteBoardBottom">
-          <div className="account text-neutral-500 hover:cursor-pointer hover:scale-110 hover:text-neutral-700 transition-all flex justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-              <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-            </svg>
-          </div>
+        <div className="teams text-neutral-500 hover:cursor-pointer hover:scale-110 hover:text-neutral-700 transition-all flex justify-center" >
+          <BsPeopleFill size={'2.25em'} 
+            onMouseEnter={() => { 
+              teamsText.current?.classList.add('block'); 
+              teamsText.current?.classList.remove('hidden'); 
+            }}
+            onMouseLeave={() => { 
+              teamsText.current?.classList.add('hidden'); 
+              teamsText.current?.classList.remove('block'); 
+            }}
+          />
+          <p className='top-1 w-28 absolute left-16 bg-neutral-800 p-1 rounded-sm text-neutral-50 text-center hidden' ref={teamsText}>Teams</p>
+        </div>
+        <div className="tasks text-neutral-500 hover:cursor-pointer hover:scale-110 hover:text-neutral-700 transition-all flex justify-center">
+          <BsListTask size={'2.5em'} 
+            onMouseEnter={() => { 
+              tasksText.current?.classList.add('block'); 
+              tasksText.current?.classList.remove('hidden'); 
+            }}
+            onMouseLeave={() => { 
+              tasksText.current?.classList.add('hidden'); 
+              tasksText.current?.classList.remove('block'); 
+            }}
+          />
+          <p className='top-1 w-28 absolute left-16 bg-neutral-800 p-1 rounded-sm text-neutral-50 text-center hidden' ref={tasksText}>Tasks</p>
+        </div>
+        <div className="alarm text-neutral-500 hover:cursor-pointer hover:scale-110 hover:text-neutral-700 transition-all flex justify-center">
+          <BsAlarmFill size={'2em'} 
+            onMouseEnter={() => { 
+              alarmText.current?.classList.add('block'); 
+              alarmText.current?.classList.remove('hidden'); 
+            }}
+            onMouseLeave={() => { 
+              alarmText.current?.classList.add('hidden'); 
+              alarmText.current?.classList.remove('block'); 
+            }}
+          />
+          <p className='top-0.5 w-28 absolute left-16 bg-neutral-800 p-1 rounded-sm text-neutral-50 text-center hidden' ref={alarmText}>Alarms</p>
+
         </div>
       </div>
-    )
+      <div className="whiteBoardBottom">
+        <div className="account text-neutral-500 hover:cursor-pointer hover:scale-110 hover:text-neutral-700 transition-all flex justify-center">
+          <BsPersonCircle size={'2.4em'} 
+            onMouseEnter={() => { 
+              accountText.current?.classList.add('block'); 
+              accountText.current?.classList.remove('hidden'); 
+            }}
+            onMouseLeave={() => { 
+              accountText.current?.classList.add('hidden'); 
+              accountText.current?.classList.remove('block'); 
+            }}
+          />
+          <p className='top-1 w-28 absolute left-16 bg-neutral-800 p-1 rounded-sm text-neutral-50 text-center hidden' ref={accountText}>Account</p>
+
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SideBar
