a77c05acb2b7e9c3b8ba4a808c1b5b5c17d94316
Josh James <110874029+joshjames289@users.noreply.github.com>
2023-05-31 18:33:17 -0700

    Josh sig form (#28)
    * created signature form component
    
    * testing + adding pdf
    
    * add jspdf
    
    * embedded 1040 pdf found on web w/ signature created from formSignature

--- src/components/form/FormSignature.tsx
+++ src/components/form/FormSignature.tsx
@@ -1,66 +1,82 @@
-import { useRef, useState } from "react";
+'use client';
+import { useState, useRef } from 'react';
+import SignatureCanvas from 'react-signature-canvas'
+import { PDFDocument } from 'pdf-lib';
 
-import SignatureCanvas from "react-signature-canvas";
 
-const FormSignature = () => {
+function FormSignature() {
   const sigCanvas = useRef<SignatureCanvas>(null);
   const [openModel, setOpenModal] = useState<boolean>(false);
   const [imageURL, setImageURL] = useState<string | null>(null);
 
-  const create = () => {
-    const URL = sigCanvas.current?.getTrimmedCanvas().toDataURL("image/png");
-    setImageURL(URL ?? "");
-    setOpenModal(false);
-  };
 
-  const download = () => {
-    const dlink = document.createElement("a");
-    dlink.setAttribute("href", imageURL ?? "");
-    dlink.setAttribute("download", "signature.png");
-    dlink.click();
+  async function create() {
+    if(sigCanvas.current) {
+      const signature = sigCanvas.current.getTrimmedCanvas().toDataURL("image/png");
+      setImageURL(signature ?? "");
+      setOpenModal(false);
+    
+      const pdfURL = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
+      const existingPdfBytes = await fetch(pdfURL).then(res => res.arrayBuffer())
+      const pdfDoc = await PDFDocument.load(existingPdfBytes)
+  
+      const pngImageBytes = await fetch(signature).then((res) => res.arrayBuffer())
+      const sigImage = await pdfDoc.embedPng(pngImageBytes)
+      const pngDims = sigImage.scale(0.5)
+    
+      const pages = pdfDoc.getPages()
+      const firstPage = pages[0]
+      // const { width, height } = firstPage.getSize()
+  
+      firstPage.drawImage(sigImage, {
+        x: firstPage.getWidth() / 2 - pngDims.width / 2,
+        y: firstPage.getHeight() / 2 - pngDims.height / 2 - 100,
+        width: pngDims.width,
+        height: pngDims.height,
+      })
+  
+      const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+      document.getElementById('pdf').src = pdfDataUri;
+    }
   };
+  
 
   return (
     <div className="form">
-      <button onClick={() => setOpenModal(true)}>Create Signature</button>
+      <button onClick={ () => setOpenModal(true) }>Create Signature</button>
       <br />
-      {imageURL && (
-        <>
-          <img src={imageURL} alt="signature" className="signature" />
-          <br />
-          <button
-            onClick={download}
-            style={{ padding: "5px", marginTop: "5px" }}
-          >
-            Download
-          </button>
-        </>
-      )}
+      {
+        imageURL && (
+          <>
+            <img src={imageURL} alt="signature" className="signature" />
+            <br />
+          </>
+        )
+      }
       <br />
 
       {openModel && (
-        <div className="modalContainer">
-          <div className="modal">
-            <div className="sigPadContainer">
-              <SignatureCanvas
-                penColor="black"
-                canvasProps={{ className: "sigCanvas" }}
-                ref={sigCanvas}
-              />
-              <hr />
-              <button onClick={() => sigCanvas.current?.clear()}>Clear</button>
-            </div>
-            <div className="modal_bottom">
+        <div className='modalContainer'>
+          <div className='modal'>
+          <div className="sigPadContainer">
+            <SignatureCanvas
+              penColor='black'
+              canvasProps={{ className: "sigCanvas" }}
+              ref={sigCanvas}
+            />
+            <hr/>
+            <button onClick={() => sigCanvas.current?.clear()}>Clear</button>
+          </div>
+            <div className='modal_bottom'>
               <button onClick={() => setOpenModal(false)}>Cancel</button>
-              <button className="create" onClick={create}>
-                Create
-              </button>
+              <button className="create" onClick={create}>Create</button>
             </div>
           </div>
         </div>
-      )}
+      )} 
+      <iframe id="pdf" height={600} width={600}></iframe>
     </div>
   );
-};
+}
 
 export default FormSignature;
