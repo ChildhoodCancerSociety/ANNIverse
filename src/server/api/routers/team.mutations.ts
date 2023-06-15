3ed9ee123dadb4f48476277eb14fa8ffec4e3bd2
Rufan Mirganiyev <109354732+rufanmirganiyev@users.noreply.github.com>
2023-06-14 20:12:02 -0400

    Created CRUD op/s for teams endpoint (#32)
    * CRUD op/s for meeting endpoint
    
    * Created CRUD op/s for meeting endpoint
    
    * Created CRUD op/s for teams endpoint

--- src/server/api/routers/team.mutations.ts
+++ src/server/api/routers/team.mutations.ts
@@ -1,5 +1,74 @@
-import { createTRPCRouter } from "../trpc";
+import { TRPCError } from "@trpc/server";
 
-const teamMutationsRouter = createTRPCRouter({});
+import { z } from "zod";
 
-export default teamMutationsRouter;
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { teamSchema } from "./team.validators";
+
+const taskMutationsRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(teamSchema)
+    .mutation(async ({ ctx: { prisma }, input }) => {
+      const { name, description } = input;
+
+      const team = await prisma.team.create({
+        data: {
+          name,
+          description,
+        },
+      });
+
+      if (!team) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to create the team.",
+        });
+      }
+
+      return team;
+    }),
+
+  update: protectedProcedure
+    .input(teamSchema.extend({ id: z.string().cuid() }))
+    .mutation(async ({ ctx: { prisma }, input }) => {
+      const { id, name, description } = input;
+
+      const team = await prisma.team.update({
+        where: { id },
+        data: {
+          name,
+          description,
+        },
+      });
+
+      if (!team) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Failed to update the team.",
+        });
+      }
+
+      return team;
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string().cuid() }))
+    .mutation(async ({ ctx: { prisma }, input }) => {
+      const { id } = input;
+
+      const team = await prisma.team.delete({
+        where: { id },
+      });
+
+      if (!team) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to delete the team.",
+        });
+      }
+
+      return true;
+    }),
+});
+
+export default taskMutationsRouter;
